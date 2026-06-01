c8e337e94d8f36eb7b5472a4062c188fa5aa15c1
medahlia <matvienko.dash.0k@gmail.com>
2026-06-01 22:02:03 +0300

    init project

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Home on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
